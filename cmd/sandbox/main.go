2685b36390a24ae473a085e2e5ab770560d6ff1d
Polo123456789 <pabsan02@gmail.com>
2024-05-31 11:32:40 -0600

    (+) Just some limit testing
    With as big of a canvas my terminal allows me to render, im  still
    getting some good 80-90 fps, without diffing. Im pretty sure this is
    done for now

--- cmd/sandbox/main.go
+++ cmd/sandbox/main.go
@@ -1,122 +1,152 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"math/rand"
 	"os"
-	"runtime/pprof"
+	"os/signal"
 	"time"
 
 	"github.com/Polo123456789/go-game/pkg/trender"
-	"github.com/Polo123456789/go-game/pkg/trender/rgb-true-color-pixels"
+	"github.com/Polo123456789/go-game/pkg/trender/8-color-pixels"
 )
 
-const (
-	Width  = 180
-	Height = 60
-)
+type Particle struct {
+	x, y, vx, vy float64
+}
 
-func run() error {
-	cpuProfileFile, err := os.Create("cpu.prof")
-	if err != nil {
-		return err
+func NewRandomParticle(width, height int) Particle {
+	return Particle{
+		x:  float64(rand.Intn(width)),
+		y:  float64(rand.Intn(height)),
+		vx: rand.Float64()*2 - 1,
+		vy: rand.Float64()*2 - 1,
 	}
-	pprof.StartCPUProfile(cpuProfileFile)
-	defer pprof.StopCPUProfile()
+}
 
-	memProfileFile, err := os.Create("mem.prof")
-	if err != nil {
-		return err
+type Simulation struct {
+	Canvas     *trender.Canvas
+	Particles  []Particle
+	Elasticity float64
+
+	background trender.Pixel
+	particle   trender.Pixel
+}
+
+func NewSimulation(
+	width, height int,
+	elasticity float64,
+	particleCount int,
+	backgroundPixel trender.Pixel,
+	particlePixel trender.Pixel,
+) *Simulation {
+	s := &Simulation{
+		Canvas:     trender.NewCanvas(width, height, backgroundPixel),
+		Elasticity: elasticity,
+		background: backgroundPixel,
+		particle:   particlePixel,
 	}
-	defer pprof.WriteHeapProfile(memProfileFile)
+	for i := 0; i < particleCount; i++ {
+		s.Particles = append(s.Particles, NewRandomParticle(width, height))
+	}
+	return s
+}
 
-	os.Stdout.WriteString(trender.AnsiClearScreen)
+func (s *Simulation) DrawParticles(pixel trender.Pixel) {
+	for _, p := range s.Particles {
+		s.Canvas.SetPixel(int(p.x), int(p.y), pixel)
+	}
+}
 
-	defaultPixel := pixels.NewPixel(
-		pixels.RGB{
-			R: 0,
-			G: 0,
-			B: 0,
-		},
-		pixels.RGB{
-			R: 0xff,
-			G: 0xff,
-			B: 0xff,
-		},
-		' ',
-	)
-	canvas := trender.NewCanvas(
-		Width,
-		Height,
-		defaultPixel,
-	)
-
-	ballPixel := pixels.NewPixel(
-		pixels.RGB{
-			R: 0,
-			G: 0,
-			B: 0,
-		},
-		pixels.RGB{
-			R: 41,
-			G: 205,
-			B: 217,
-		},
-		' ',
-	)
-
-	ballX := float64(Width / 2)
-	ballY := float64(1)
-	ballVelocity := 0.0
-
-	gravity := 0.2
-
-	timeSpentSum := int64(0)
-	framesCount := 0
-	maxFrames := 1000
-
-	for {
-
-		canvas.SetPixel(
-			int(ballX),
-			int(ballY),
-			ballPixel,
-		)
-
-		framesCount++
-		start := time.Now()
-		canvas.RenderFull()
-		timeSpentSum += time.Since(start).Milliseconds()
-
-		if framesCount >= maxFrames {
-			os.Stdout.WriteString(trender.SetCursorPosition(0, 27))
-			fmt.Printf("Frames Count: %v\n", framesCount)
-			fmt.Printf("Average TimeSpent on Render: %v", timeSpentSum/int64(maxFrames))
-			return nil
+func (s *Simulation) UpdateParticles() {
+	for i := range s.Particles {
+		p := &s.Particles[i]
+		p.x += p.vx
+		p.y += p.vy
+		if p.x < 0 {
+			p.x = 0
+			p.vx = -p.vx * s.Elasticity
 		}
-
-		canvas.SetPixel(
-			int(ballX),
-			int(ballY),
-			defaultPixel,
-		)
-
-		ballY += ballVelocity
-		ballVelocity += gravity
-
-		if ballY < 0 {
-			ballY = 0
-			ballVelocity = -ballVelocity
+		if p.x >= float64(s.Canvas.Width()) {
+			p.x = float64(s.Canvas.Width()) - 1
+			p.vx = -p.vx * s.Elasticity
 		}
-
-		if ballY >= Height {
-			ballY = Height - 1
-			ballVelocity = -ballVelocity * 0.8
+		if p.y < 0 {
+			p.y = 0
+			p.vy = -p.vy * s.Elasticity
+		}
+		if p.y >= float64(s.Canvas.Height()) {
+			p.y = float64(s.Canvas.Height()) - 1
+			p.vy = -p.vy * s.Elasticity
 		}
 	}
 }
 
+func run(ctx context.Context, args []string) error {
+	flags := flag.NewFlagSet("game", flag.ExitOnError)
+	width := flags.Int("width", 450, "Width of the canvas")
+	height := flags.Int("height", 80, "Height of the canvas")
+	particleCount := flags.Int("particles", 1000, "Number of particles")
+	elasticity := flags.Float64("elasticity", 0.9, "Elasticity of the particles")
+	flags.Parse(args[1:])
+
+	s := NewSimulation(
+		*width, *height,
+		*elasticity,
+		*particleCount,
+		pixels.NewPixel(
+			pixels.Black,
+			pixels.White,
+			pixels.Reset,
+			' ',
+		),
+		pixels.NewPixel(
+			pixels.White,
+			pixels.Black,
+			pixels.Reset,
+			' ',
+		),
+	)
+
+	start := time.Now()
+	simStart := time.Now()
+	var renderTimeSum time.Duration
+	var frameCount int
+
+	for ctx.Err() == nil {
+		s.DrawParticles(s.background)
+		s.UpdateParticles()
+		s.DrawParticles(s.particle)
+		spent := time.Since(start)
+		frameCount++
+		renderTimeSum += spent
+		s.Canvas.RenderFull()
+		os.Stdout.WriteString(trender.SetCursorPosition(0, s.Canvas.Height()+1))
+		start = time.Now()
+	}
+
+	os.Stdout.WriteString(trender.SetCursorPosition(0, s.Canvas.Height()+1))
+	fmt.Printf("Frames rendered: %d\n", frameCount)
+	fmt.Printf("Average render time: %v\n", renderTimeSum/time.Duration(frameCount))
+	fmt.Printf("Simulation time: %v\n", time.Since(simStart))
+
+	return nil
+}
+
 func main() {
-	if err := run(); err != nil {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
+	go func() {
+		<-sigchan
+		cancel()
+	}()
+
+	if err := run(ctx, os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
